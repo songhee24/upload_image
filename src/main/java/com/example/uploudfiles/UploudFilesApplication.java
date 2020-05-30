96b83960f7287b22be0f882ac95fd7f5b5ddc774
Azamat Orozbaev <44497623+songhee24@users.noreply.github.com>
2020-05-30 16:11:24 +0600

    start

---
+++ src/main/java/com/example/uploudfiles/UploudFilesApplication.java
@@ -0,0 +1,13 @@
+package com.example.uploudfiles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UploudFilesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UploudFilesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/uploudfiles/UploudFilesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.uploudfiles;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UploudFilesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
